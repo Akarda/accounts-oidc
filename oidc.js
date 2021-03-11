6c581876470ace4b7664c2beaefaca506012f665
Andreas Schormüller <whitekelt@googlemail.com>
2021-03-11 19:43:33 +0100

    publish version

---
+++ oidc.js
@@ -0,0 +1,22 @@
+Accounts.oauth.registerService('azureAd')
+
+if (Meteor.isClient) {
+  Meteor.loginWithAzureAd = function (options, callback) {
+    // support a callback without options
+    if (!callback && typeof options === 'function') {
+      callback = options
+      options = null
+    }
+
+    var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback)
+    Oidc.requestCredential(options, credentialRequestCompleteCallback)
+  }
+} else {
+  Accounts.addAutopublishFields({
+    // not sure whether the OIDC api can be used from the browser,
+    // thus not sure if we should be sending access tokens; but we do it
+    // for all other oauth2 providers, and it may come in handy.
+    forLoggedInUser: ['services.azureAd'],
+    forOtherUsers: ['services.azureAd.id']
+  })
+}
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary: 'OpenID Connect (OIDC) for Meteor accounts',
+  version: '1.0.10',
+  name: 'akarda:accounts-oidc',
+  git: 'https://github.com/akarda/accounts-oidc.git'
+
+})
+
+Package.onUse(function (api) {
+  api.use('accounts-base@1.2.0', ['client', 'server'])
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server'])
+  api.use('accounts-oauth@1.1.0', ['client', 'server'])
+  api.use('akarda:oidc@1.0.13', ['client', 'server'])
+
+  api.addFiles('oidc_login_button.css', 'client')
+
+  api.addFiles('oidc.js')
+})
