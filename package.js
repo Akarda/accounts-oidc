538f5be8ea3137d7b9fd7d5a38ef88e9e83ff49f
Andreas Schormüller <whitekelt@googlemail.com>
2021-03-12 01:59:31 +0100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'OpenID Connect (OIDC) for Meteor accounts',
-  version: '1.0.10',
+  version: '1.0.11',
   name: 'akarda:accounts-oidc',
   git: 'https://github.com/akarda/accounts-oidc.git'
 
@@ -11,7 +11,7 @@ Package.onUse(function (api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server'])
   api.use('accounts-oauth@1.1.0', ['client', 'server'])
-  api.use('akarda:oidc@1.0.13', ['client', 'server'])
+  api.use('akarda:oidc@1.0.14', ['client', 'server'])
 
   api.addFiles('oidc_login_button.css', 'client')
 
